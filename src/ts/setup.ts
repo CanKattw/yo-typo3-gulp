2e521fb89678e4ae405560d97d8d0f061bcfffda
Can Kattw <ck115@hdm-stuttgart.de>
2015-08-15 19:34:30 +0200

    cleanup

--- src/ts/setup.ts
+++ src/ts/setup.ts
@@ -1,4 +1,2 @@
 // include this into your TypoScript to load the TypoScript from our files
 <INCLUDE_TYPOSCRIPT: source="DIR:fileadmin/template/includes/" extensions="ts">
-// include this in your user.ts (see README for more information)
-// so typo3 doesn not cache our typoscript files
